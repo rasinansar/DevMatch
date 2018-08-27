dbcd40bc5fb7842e0e9e4c92fb2997081108fc09
Rasin Ansar <rasinansar6@gmail.com>
2018-08-27 14:56:28 +0000

    Added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,18 @@
 class ContactsController < ApplicationController
+ 
+ # GET /contact-us
+ # Show contact form
   def new
     @contact = Contact.new
   end
   
-  
+  # POST /contacts
   def create 
+    # Mass assignment of form fields into Contact object
     @contact = Contact.new(contact_params)
+    # Save the Contact object to the database
     if @contact.save
+      # Store form fields via parameters
       flash[:success] = "Message sent"
       redirect_to new_contact_path
     else
